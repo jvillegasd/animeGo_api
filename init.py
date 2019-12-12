66e891fa4436dfaaf0542c9673ac7d452638cc2f
C9-LinkRs <linkrsfan@gmail.com>
2019-12-12 03:46:20 +0100

    Docker images added

--- init.py
+++ init.py
@@ -11,4 +11,4 @@ api.add_namespace(animeflv_api, path='/api/AnimeFLV')
 api.add_namespace(jkanime_api, path='/api/JKanime')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
